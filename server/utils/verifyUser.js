16779de7b804613a68064d0b05dc7c8d6e4320e5
ramazangoren <rmzngrn0404@gmail.com>
2024-09-13 18:09:43 +0300

    cors problem

--- server/utils/verifyUser.js
+++ server/utils/verifyUser.js
@@ -1,64 +1,10 @@
-// import jwt from "jsonwebtoken";
-
-// const verifyToken = (req, res, next) => {
-//   const token = req.cookies.access_token;
-
-//   if (!token) {
-//     return res
-//       .status(401)
-//       .json({ success: false, message: "Access denied. No token provided." });
-//   }
-
-//   try {
-//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-//     req.userId = decoded.id;
-//     // console.log(decoded);
-    
-//     next();
-//   } catch (err) {
-//     res.status(400).json({ success: false, message: "Invalid token." });
-//   }
-// };
-
-// export default verifyToken;
-
-
-// const verifyToken = (req, res, next) => {
-//   const token = req.cookies.access_token;
-
-//   console.log('Cookies:', req.cookies.access_token); // Debug log to check if cookies are received
-
-//   if (!token) {
-//     return res
-//       .status(401)
-//       .json({ success: false, message: "Access denied. No token provided." });
-//   }
-
-//   try {
-//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-//     req.userId = decoded.id;
-//     console.log('Decoded token:', decoded); // Debug log decoded token
-    
-//     next();
-//   } catch (err) {
-//     console.error('JWT Error:', err); // Log error for debugging
-//     res.status(400).json({ success: false, message: "Invalid token." });
-//   }
-// };
-
-
-// export default verifyToken;
-
-
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
 const verifyToken = (req, res, next) => {
-  // Extract the token from cookies
-  const cookies = req.cookies;
-  const token = cookies && cookies.access_token;
-
-  console.log('Cookies:', token); // Debug log to check if cookies are received
-
+  const token = req.cookies.access_token;
+  console.log("toke", token); // Debug log to check if cookies are received
+  console.log("Cookies:", req.cookies.access_token); // Debug log to check if cookies are received
+  console.log("req.Cookies:", req.cookies);
   if (!token) {
     return res
       .status(401)
@@ -66,18 +12,14 @@ const verifyToken = (req, res, next) => {
   }
 
   try {
-    // Verify the token
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.userId = decoded.id;
-    console.log('Decoded token:', decoded); // Debug log decoded token
-    
+    console.log("Decoded token:", decoded); // Debug log decoded token
+
     next();
   } catch (err) {
-    console.error('JWT Error:', err); // Log error for debugging
     res.status(400).json({ success: false, message: "Invalid token." });
   }
 };
 
 export default verifyToken;
-
-
